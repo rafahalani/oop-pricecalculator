0cef51fee5ebaea03c22c3f0ea117ae8fda98c95
rafahalani <alani.rafah@yahoo.com>
2019-10-16 16:58:49 +0200

    add methods

--- model/ProductLoader.php
+++ model/ProductLoader.php
@@ -3,5 +3,14 @@ declare(strict_types=1);
 
 class ProductLoader
 {
+    public function loadProduct(): array
+    {
+        $list = [];
+        $json = json_decode(file_get_contents('data/products.json'),true);
+        foreach ($json AS $productJson){
+            $list[] = new Product($productJson['id'],$productJson['name'],$productJson['description'],$productJson['price']);
 
+        }
+        return $list;
+    }
 }
